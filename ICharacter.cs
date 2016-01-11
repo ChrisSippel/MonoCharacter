1e34161df67b18b3636bfa038a2e2f058225d12f
Chris Sippel <Chris_Sippel@hotmail.com>
2016-01-11 17:41:28 -0500

    Added the initial code
    Added the initial code to the project

---
+++ ICharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoCharacter
+{
+    public interface ICharacter
+    {
+        void Initialize();
+
+        void LoadContent(ContentManager manager);
+
+        void UnloadContent();
+
+        void Update(GameTime gameTime);
+
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
